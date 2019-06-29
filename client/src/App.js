38b1ef0f39265b709db38f2f973e1a1956b70c26
Junwon Jung <junwon3329@gmail.com>
2019-06-29 18:49:35 -0500

    Reset file trying to revert it back

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/navbar';
+import Home from './components/home';
+import { SignIn, SignUp } from './components/auth';
+import NoMatch from './components/nomatch';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/signin' component={SignIn} />
+            <Route exact path='/signup' component={SignUp} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
