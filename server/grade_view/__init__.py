834ab9a166b44e4ce9160fc8420252399d6fa243
Junwon Jung <junwon3329@gmail.com>
2019-06-30 14:34:17 -0500

    Signin, Signout, Sign up implementation using flask-login has been completed

--- server/grade_view/__init__.py
+++ server/grade_view/__init__.py
@@ -7,10 +7,11 @@ db_path = os.path.abspath(os.getcwd()) + '/grade_view/database/grade_view.db'
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = b'\x10wEL\x1d\xba\xec\x9cge\xa8\xb15\xf3GO\x0b\x8c\xb9\xf6\xbf\x02R\xf3'
 
 db = SQLAlchemy(app)
 
-CORS(app)
+CORS(app,supports_credentials=True)
 
 from grade_view.setup_database import database_setup
 from grade_view.models import User, MajorUserRelationship, Major
--- server/grade_view/blue_prints/auth.py
+++ server/grade_view/blue_prints/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user, login_required,logout_user
 from grade_view import db, app
-from grade_view.models import User, UserSchema, Major, MajorSchema,MajorUserRelationship
+from grade_view.models import User, UserSchema, Major, MajorSchema, MajorUserRelationship
 import re
 
 auth = Blueprint('auth', __name__)
@@ -13,12 +13,42 @@ login_manager.login_view = "login"
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    return User.query.get(int(user_id))
 
 
-@auth.route('/login')
+@auth.route('/authenticated')
+def authenticated():
+    return jsonify({
+        'status': current_user.is_authenticated,
+        'user': UserSchema().dump(current_user).data if current_user.is_authenticated else None
+    })
+
+
+@auth.route('/signin', methods=['POST'])
 def login():
-    return 'Login'
+    content = request.get_json()
+    email = content['email']
+    password = content['password']
+    try:
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            raise Exception('Email does not exists')
+        if not user.check_password(password):
+            raise Exception('Password does not match')
+        print(current_user)
+        if current_user.is_authenticated:
+            raise Exception('User is already logged in')
+        login_user(user)
+        return jsonify({
+            'status': True,
+            'user': UserSchema().dump(user).data
+        })
+    except Exception as e:
+        return jsonify({
+            'status': False,
+            'message': str(e)
+        })
+
 
 @auth.route('/majors')
 def get_majors():
@@ -49,7 +79,8 @@ def signup():
         db.session.commit()
         # put into MajorUserRelationship object to add in database
         for major in content['majors']:
-            db.session.add(MajorUserRelationship(user.id,Major.query.filter_by(major=major).first().id))
+            db.session.add(MajorUserRelationship(
+                user.id, Major.query.filter_by(major=major).first().id))
         db.session.commit()
 
         return jsonify({'status': True, 'user': UserSchema().dump(user).data})
@@ -57,9 +88,20 @@ def signup():
         return jsonify({'status': False, 'message': str(e)})
 
 
-@auth.route('/logout')
+@auth.route('/signout')
 def logout():
-    return 'Logout'
+    if current_user.is_authenticated:
+        logout_user()
+        return jsonify({
+        'status': True,
+        'message': 'Successfully logged out'
+    })
+    else:
+        return jsonify({
+        'status': False,
+        'message': 'User is not authenticated'
+    })
+
 
 
 app.register_blueprint(auth, url_prefix='/auth')
