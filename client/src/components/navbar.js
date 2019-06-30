b867aa47d62f396226634fc45df21c1f0080d6e8
Junwon Jung <junwon3329@gmail.com>
2019-06-30 14:31:40 -0500

    Based on the authentication information, it responsively changes signing in up out

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import { Link } from 'react-router-dom'
+import AuthContext from '../contexts/auth_context';
+import Axios from 'axios';
+import { SERVER, PORT } from '../config';
 
 const Navbar = () => {
+
+    const { user, setUser } = useContext(AuthContext);
+
+    const handleSignOut = async () => {
+        try {
+            const { status } = (await Axios.get(`${SERVER}:${PORT}/auth/signout`)).data
+            if (status) setUser(null);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <nav className="navbar navbar-light bg-light">
             <div className="container">
                 <Link className="navbar-brand" to="/">Grade View</Link>
                 <div className="form-inline my-2 my-lg-0" >
-                    <Link className="navbar-brand" to="/signup">Sign up</Link>
-                    <Link className="navbar-brand" to="/login">Sign in</Link>
-                    <Link className="navbar-brand" to="/logout">Sign out</Link>
+                    {
+                        user ? (
+                            <Link onClick={handleSignOut} to='#' className="navbar-brand">Sign out</Link>
+                        ) : (
+                                <Fragment>
+                                    <Link className="navbar-brand" to="/signup">Sign up</Link>
+                                    <Link className="navbar-brand" to="/signin">Sign in</Link>
+                                </Fragment>
+                            )
+                    }
+
                 </div>
             </div>
         </nav >
