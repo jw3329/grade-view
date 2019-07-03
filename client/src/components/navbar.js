543410f37281d3014e1788f90d68b1f2e672aca2
Junwon Jung <junwon3329@gmail.com>
2019-07-03 16:32:34 -0500

    Added register gpa navbar tab

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,7 +2,7 @@ import React, { Fragment, useContext } from 'react';
 import { Link } from 'react-router-dom'
 import AuthContext from '../contexts/auth_context';
 import Axios from 'axios';
-import { SERVER, PORT } from '../config';
+import { SERVER } from '../config';
 
 const Navbar = () => {
 
@@ -10,7 +10,7 @@ const Navbar = () => {
 
     const handleSignOut = async () => {
         try {
-            const { status } = (await Axios.get(`${SERVER}:${PORT}/auth/signout`)).data
+            const { status } = (await Axios.get(`${SERVER}/auth/signout`)).data
             if (status) setUser(null);
         } catch (error) {
             console.log(error);
@@ -25,6 +25,7 @@ const Navbar = () => {
                     {
                         user ? (
                             <Fragment>
+                                <Link to='/register/gpa' className="navbar-brand">Register GPA</Link>
                                 <Link onClick={handleSignOut} to='#' className="navbar-brand">Sign out</Link>
                                 <Link className="navbar-brand" to='/settings/profile'>Settings</Link>
                             </Fragment>
