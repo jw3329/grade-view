ae5f5b636a75705d3156bb30a05d9c522e603696
Junwon Jung <junwon3329@gmail.com>
2019-08-28 00:19:57 -0500

    From direct gpa number, created logic to convert to grade to map into GPA

--- server/grade_view/blue_prints/register.py
+++ server/grade_view/blue_prints/register.py
@@ -28,8 +28,9 @@ def profile():
         try:
             course = registered_data['course']
             course_number = registered_data['course_number']
+            # handle course number if it is more than 3 digits
+            if len(course_number) > 3: raise Exception('Invalid course number')
             gpa = registered_data['gpa']
-            print(Major.query.filter_by(major=course).first().id)
             major_id = Major.query.filter_by(major=course).first().id
             course_data = Course.query.filter_by(major_id=major_id).filter_by(course_number=course_number).first()
             # if there's no filter result, then add it
@@ -37,9 +38,25 @@ def profile():
                 course_data = Course(major_id,course_number)
                 db.session.add(course_data)
                 db.session.commit()
-            # after commit, add gpa data
-            print(course_data)
-            gpa_data = GPA(current_user.id, course_data.id, gpa)
+            # after commit, add gpa data        
+            grade_map = {
+                'A+/A': 4.00,
+                'A-': 3.67,
+                'B+': 3.33,
+                'B': 3.00,
+                'B-': 2.67,
+                'C+': 2.33,
+                'C': 2.00,
+                'C-': 1.67,
+                'D+': 1.33,
+                'D': 1.00,
+                'D-': 0.67,
+                'F': 0.00,
+            }
+
+            # handle error case if the grade does not belong to the category
+            if gpa not in grade_map: raise Exception('Given grade does not belong to the grade map')
+            gpa_data = GPA(current_user.id, course_data.id, grade_map[gpa])
             db.session.add(gpa_data)
             db.session.commit()
             status = True
