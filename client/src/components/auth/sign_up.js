f1d6e43df6232d271a50a05d541e9a6377cbe971
Junwon Jung <junwon3329@gmail.com>
2019-08-27 21:13:55 -0500

    Added majors that is needed to acknowledge when signing up

--- client/src/components/auth/sign_up.js
+++ client/src/components/auth/sign_up.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { SERVER } from '../../config';
 
-const SignUp = ({ history }) => {
+const SignUp = () => {
 
     const [form, setForm] = useState({
         email: '',
