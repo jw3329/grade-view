115a105cb7d1f73f65150a28bba404fde93c1ea0
Junwon Jung <junwon3329@gmail.com>
2019-09-08 17:11:49 -0500

    Api calling methods which is calling follow data information

--- server/grade_view/blue_prints/api.py
+++ server/grade_view/blue_prints/api.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, jsonify,send_file
 from grade_view import db, app, UPLOAD_FOLDER
-from grade_view.models import User, GPA, UserSchema
+from grade_view.models import User, GPA, UserSchema,Follow
 import os
+from flask_login import login_required, current_user
 
 api = Blueprint('api', __name__)
 
@@ -62,6 +63,89 @@ def api_profile_image(filename):
         return jsonify({
             'status':status,
             'message': message
+        })
+
+@api.route('/follow',methods=['POST'])
+@login_required
+def follow():
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+            destination_username = data['destination_username']
+            user = User.query.filter_by(username=destination_username).first()
+            if not user: raise Exception('No user found to follow')
+            # find if current user is following the user or not
+            follow = Follow.query.filter(Follow.source_id == current_user.id, Follow.destination_id == user.id).first()
+            # if no data exists, follow the user, if it exists, then delete the follow data
+            if not follow:
+                db.session.add(Follow(current_user.id, user.id))
+            else:
+                db.session.delete(follow)
+            db.session.commit()
+            status = True
+            message = None
+        except Exception as e:
+            status = False
+            message = str(e)
+        return jsonify({
+            'status':status,
+            'message': message
+        })
+
+
+@api.route('/get_followers',methods=['POST'])
+@login_required
+def get_followers():
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+            username = data['username']
+            user = User.query.filter_by(username=username).first()
+            if not user: raise Exception('No user found')
+            # find if current user is following the user or not
+            result_data = Follow.query.filter(Follow.destination_id == user.id).all()
+            # if no data exists, follow the user, if it exists, then delete the follow data
+            followers = []
+            for follower_data in result_data:
+                follower = User.query.get(follower_data.source_id)
+                followers.append(follower.username)
+            status = True
+            message = None
+        except Exception as e:
+            status = False
+            message = str(e)
+        return jsonify({
+            'status':status,
+            'message': message,
+            'followers': followers
+        })
+
+
+@api.route('/get_following_users',methods=['POST'])
+@login_required
+def get_following_users():
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+            username = data['username']
+            user = User.query.filter_by(username=username).first()
+            if not user: raise Exception('No user found')
+            # find if current user is following the user or not
+            result_data = Follow.query.filter(Follow.source_id == user.id).all()
+            # if no data exists, follow the user, if it exists, then delete the follow data
+            following_users = []
+            for following_user_data in result_data:
+                following_user = User.query.get(following_user_data.destination_id)
+                following_users.append(following_user.username)
+            status = True
+            message = None
+        except Exception as e:
+            status = False
+            message = str(e)
+        return jsonify({
+            'status':status,
+            'message': message,
+            'following_users': following_users
         })
 
 app.register_blueprint(api, url_prefix='/api')
